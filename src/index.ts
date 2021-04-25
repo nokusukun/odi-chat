81967e47a95a797a55c6448ba4ddcc9534fc4084
Natsuko Higashikuni <nokusukun@yahoo.co.jp>
2021-04-25 17:29:49 +0800

    updated kerfuffle file

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ new Core({
   sources: __dirname,
   server: {
     socket: true,
-    port: 8080,
+    port: Number(process.env.APP_PORT || 8051),
     static: {
       root: join(__dirname, "../assets"),
       prefix: "/assets"
